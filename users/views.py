904c664a049caebce9c2eca0ffbc8b38e075b85b
aliyurekli32 <adamjesperblack@gmail.com>
2023-01-04 10:57:55 -0800

    Feat: Add new registration methods email and password

--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
 
-# Create your views here.
+class RegisterAPI(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
