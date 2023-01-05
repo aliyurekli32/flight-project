b87ee4c54d3c6d9312d92243bc638f818a58237a
aliyurekli32 <adamjesperblack@gmail.com>
2023-01-05 08:47:48 -0800

    Add: Flights new files

--- flight/admin.py
+++ flight/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Flight, Passenger, Reservation
+
+# Register your models here.
+admin.site.register(Flight)
+admin.site.register(Passenger)
+admin.site.register(Reservation)
